b97009de37958dc84de8cf9583511980b877e2b5
SleepNoMore <nninkovic.py@gmail.com>
2020-10-10 09:27:18 +0200

    Added image field to recipe model

--- src/recipe/urls.py
+++ src/recipe/urls.py
@@ -7,8 +7,8 @@ router = DefaultRouter()
 router.register("tags", views.TagViewSet)
 router.register("ingredients", views.IngredientViewSet)
 router.register("recipes", views.RecipeViewSet)
-# Path name is 'recipe-list' and 'recipe-detail'
-# RECIPES_URL = reverse("recipe:recipe-list")
+# Path name is 'recipe-list' and 'recipe-detail'.
+# RECIPES_URL = reverse("recipe:recipe-list") 'recipe' in an app_name.
 # RECIPES_DETAIL_URL = reverse("recipe:recipe-detail", args=[recipe_id]) in a function.
 
 app_name = "recipe"
